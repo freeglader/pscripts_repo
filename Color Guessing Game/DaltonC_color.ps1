c4a9d1603217f96a92bf70813f1caf5950be092d
freeglader <daltonsolo@protonmail.com>
2018-05-24 20:42:17 -0700

    created repo

---
+++ Color Guessing Game/DaltonC_color.ps1
@@ -0,0 +1,26 @@
+#===================================================
+# Program Name : Color
+# Author: Dalton Cole
+# I, Dalton Cole, wrote this script as original work completed by me.
+# Special Feature:  <describe your special feature here>
+#===================================================
+# Hashtables for all the console colors
+$Black=@{color = "Black"; guessed = $false}
+$DarkBlue=@{color = "DarkBlue"; guessed = $false}
+$DarkGreen=@{color = "DarkGreen"; guessed = $false}
+$DarkCyan=@{color = "DarkCyan"; guessed = $false}
+$DarkRed=@{color = "DarkRed"; guessed = $false}
+$DarkMagenta=@{color = "DarkMagenta"; guessed = $false}
+$DarkYellow=@{color = "DarkYellow"; guessed = $false}
+$Gray=@{color = "Gray"; guessed = $false}
+$DarkGray=@{color = "DarkGray"; guessed = $false}
+$Blue=@{color = "Blue"; guessed = $false}
+$Green=@{color = "Green"; guessed = $false}
+$Cyan=@{color = "Cyan"; guessed = $false}
+$Red=@{color = "Red"; guessed = $false}
+$Magenta=@{color = "Magenta"; guessed = $false}
+$Yellow=@{color = "Yellow"; guessed = $false}
+$White=@{color = "White"; guessed = $false}
+
+$list = $Black,$DarkBlue,$DarkGreen,$DarkCyan,$DarkRed,$DarkMagenta,$DarkYellow,$Gray,$DarkGray,$Blue,$Green,$Cyan,$Red,$Magenta,$Yellow,$White
+
