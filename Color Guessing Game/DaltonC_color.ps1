94469f92525ede305707fe844809c3c5cdb95278
freeglader <27751160+freeglader@users.noreply.github.com>
2018-05-26 19:50:20 -0700

    Menu works. Just need to work on the end game stats and timer

--- Color Guessing Game/DaltonC_color.ps1
+++ Color Guessing Game/DaltonC_color.ps1
@@ -9,7 +9,8 @@
 $validColors= "Black","DarkBlue","DarkGreen","DarkCyan","DarkRed","DarkMagenta","DarkYellow","Gray","DarkGray","Blue","Green","Cyan","Red","Magenta","Yellow","White"
 $attempts = 0
 [System.Collections.ArrayList]$guessedWrong = @()
-$targetPrompt = "guessPrompt"
+$targetGuess = "guessPrompt"
+
 
 function Show-Menu
 {
@@ -19,11 +20,11 @@ function Show-Menu
      cls
      Write-Host "================ $Title ================"
     
-     Write-Host "1: Press '1' to make your guess."
+     Write-Host "1: Press '1' to return to game"
      Write-Host "2: Press '2' to list valid colors"
-     Write-Host "3: Press '3' to list colors you have guessed for this round."
+     Write-Host "3: Press '3' to list colors you have guessed for this round"
      Write-Host "4: Press '4' to reveal a hint"
-     Write-Host "Q: Press 'Q' to quit round."
+     Write-Host "Q: Press 'Q' to quit round"
 }
 
 # Begin the round
@@ -46,23 +47,44 @@ do {
             Write-Host "Your guess must be one of the valid colors."
             continue
         }
+        # display menu if user presses 'm' at prompt
         if ($guess -eq "m") {
+            # this ":menuPrompt" line is a target to return to when "continue $targetname" is called in the loop
             do {
+                # call the menu function
                 Show-Menu
                 $input = Read-Host "Please make a selection"
+                # all menu options and respective code
                 switch ($input) {
                     '1' {
+                        # return to game
                         cls
-                        continue $targetPrompt
+                        # brings user back to the guess prompt with ":guessPrompt" target
+                        continue $targetGuess
                     } '2' {
+                        # list valid colors
                         cls
-                        'You chose option #2'
+                        foreach ($color in $validColors) {Write-Host $color -ForegroundColor $color}
+                        continue
                     } '3' {
+                        # list guesses made this round
                         cls
-                        'You chose option #3'
+                        # check if user has made any guesses and provide appropriate response                        
+                        if ($guessedWrong.length -ne 0) {
+                            "Colors guessed this round:"
+                            foreach ($color in $guessedWrong) {Write-Host $color -ForegroundColor $color}
+                            continue
+                        }
+                        else {
+                            "You have not made any guesses. Please return to game to make your guess"
+                            continue $targetGuess
+                        }
+                        
                     } '4' {
+                        # provide user with hint (1st letter of color)
                         cls
-                        'You chose option #4'
+                        "My favorite color begins with the letter '" + ($randomColor.Substring(0,1)) + "'"
+                        continue
                     } 'q' {
                         return
                     }
@@ -84,7 +106,7 @@ do {
     }
     until ($guess -eq $randomColor)
     if ($guess -eq $randomColor) {
-        Write-Host "Congrats you won! My favorite color was " -NoNewline
+        Write-Host "Congrats, you won! My favorite color was " -NoNewline
         Write-Host $randomColor -ForegroundColor $randomColor -NoNewline
         Write-Host ". You guessed incorrectly" ($attempts -1) -NoNewline
         Write-Host " time(s), and guessed correctly on try number" $attempts
