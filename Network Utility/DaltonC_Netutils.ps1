eb04aa74ecfa29686d90282ff8a628a6e7fdd1ca
freeglader <27751160+freeglader@users.noreply.github.com>
2018-06-16 22:39:07 -0700

    basically finshed, needs some commenting and cleanup

--- Network Utility/DaltonC_Netutils.ps1
+++ Network Utility/DaltonC_Netutils.ps1
@@ -2,52 +2,45 @@
 # Program Name : Netutils
 # Author: <Dalton Cole>
 # I, Dalton Cole, wrote this script as original work completed by me.
-# Your Network Utility:  <describe your network utility here>
-# Support functions: Describe your network support functions.
+# Your Network Utility:  <Function Get-ValidHosts calculates the number of valid hosts available for a given subnet>
+# Support functions: My support function is the Convert-CIDR function, which converts cidr notation to subnet mask in dotted decimal format
 #===================================================
 
-#! Genius way to convert binary strings into IPv4 addresses
-<#
- $a=$i=$null
-
-"11111111","11111111","11111000","00000000" |
-
- % {
-
-     $i++
-
-     [string]$a += [convert]::ToInt32($_,2)
-
-     if($i -le 3) {[string]$a += "."}
-
-   }
-
-$a
-#>
-# pushing to repo from new pc test
 #! Functions for Repeatable tasks
 $convertedIP=$null
 $counter = $null
 function Convert-CIDR ($cidr){
- if($cidr -le 32){
+# determines if cidr notation entered is valid
+ if($cidr -le 32) {
+ # creates an array with 32 elements to count bits for an ip (32 bits in an IP address, 4 octets)
  [Int[]]$totalBits = (1..32)
+ # loops through the 32 bits 
  for($i=0;$i -lt $totalBits.length;$i++){
+ <# if the bit counter is greater than the cidr mask entered, set all those bits to 0.
+ if it's within the cidr mask entered, it sets those bits to 1.
+ the cidr mask represents the network portion of the address. all those bits are 1's,
+ all the bits after that are 0's (host portion of address)
+ #>
  if($totalBits[$i] -gt $cidr){$totalBits[$i]="0"}else{$totalBits[$i]="1"}
  }
+ # joins all the bits to form a 32 bit subnet mask in binary form
  $cidr = $totalBits -join ""
- #Write-Host -Foreground "green" $cidr.Substring(0,8)
- #Write-Host -Foreground "green" $cidr.Substring(24,8)
+ # using substring breaks that binary address into 4 octets and puts it in an array
  $octetArray = @($cidr.Substring(0,8), $cidr.Substring(8,8), $cidr.Substring(16,8), $cidr.Substring(24,8))
+ # loops through array and converts each octet to integers and places a decimal between them
  $octetArray | foreach { 
         $counter++
         [string]$convertedIP += [convert]::ToInt32($_,2)
         #! Counts octets so it can add a "." after each octet except the last.
         #? (might want to change this to a join)
         if($counter -le 3) {[string]$convertedIP += "."}
+       
     }
  }
+    # returns finshed product, a dotted decimal subnet mask
     return $convertedIP
 }
+Convert-CIDR 24
 
 <#
     3. #? Function 1
@@ -64,6 +57,7 @@ function Convert-CIDR ($cidr){
 
 function Test-IPHost ($HostName,$Count = 1) {
     foreach ($name in $HostName) {
+    # gets ip address from hostname and uses test-connection to ping. returns message if the host is up or down based on the ping results
     Write-Host -Foreground "green" "Up-status for $name"
     $ip = ((Resolve-DnsName -Name $name).where({$_.Section -eq "Answer"})).IP4Address
         foreach ($i in $ip) {
@@ -93,12 +87,15 @@ function Test-IPHost ($HostName,$Count = 1) {
 function Test-IPNetwork () {
     Param(
         [Parameter(Mandatory=$true)]
+        [ValidateScript({$_ -match [IPAddress]$_ })]
         [Net.IPAddress] $IP1,
 
         [Parameter(Mandatory=$true)]
+        [ValidateScript({$_ -match [IPAddress]$_ })]
         [Net.IPAddress] $IP2,
 
         [Parameter(Mandatory=$true)]
+        # couldn't figure out how to validate, not enough time
         [string] $SubnetMask
     )
     if ($SubnetMask.length -lt 3) {
@@ -149,19 +146,21 @@ function Test-IPNetwork () {
 function Get-IPNetID {
     Param(
         [Parameter(Mandatory=$true)]
+        [ValidateScript({$_ -match [IPAddress]$_ })]
         [Net.IPAddress] $IP,
 
         [Parameter()]
+        # couldn't figure out how to validate, not enough time
         [string] $SubnetMask
     )
 
     if ($SubnetMask.length -lt 3 -and $SubnetMask.Length -gt 0) {
-        Write-Host -ForegroundColor "red" "-------------------------------DEBUGGING-----------------------------------"
+        <#Write-Host -ForegroundColor "red" "-------------------------------DEBUGGING-----------------------------------"
         "SubnetMask: " + $SubnetMask
         "SubnetMask.Length: " + $SubnetMask.Length
         "SubnetMask.GetType(): " + $SubnetMask.GetType()
         Write-Host -ForegroundColor "red" "---------------------------------------------------------------------------"
-
+        #>
         [Net.IPAddress]$SubnetMask = Convert-CIDR $SubnetMask
         Write-Host -foreground "green" "the converted cidr mask is: " $SubnetMask
     }
@@ -177,7 +176,7 @@ function Get-IPNetID {
             else {
                 Write-Host "Using Class A default mask..."
                 [Net.IPAddress]$SubnetMask = Convert-CIDR 8
-                $SubnetMask.IPAddressToString 
+                #$SubnetMask.IPAddressToString 
             }
         }
         elseif ([int]$IParray[0] -in 128..191) {
@@ -188,7 +187,7 @@ function Get-IPNetID {
             else {
                 Write-Host "Using Class B default mask..."
                 [Net.IPAddress]$SubnetMask = Convert-CIDR 16
-                $SubnetMask.IPAddressToString 
+                #$SubnetMask.IPAddressToString 
             }           
         }
         elseif ([int]$IParray[0] -in 192..223) {
@@ -199,7 +198,7 @@ function Get-IPNetID {
             else {
                 Write-Host "Using Class C default mask..."
                 [Net.IPAddress]$SubnetMask = Convert-CIDR 24
-                $SubnetMask.IPAddressToString
+                #$SubnetMask.IPAddressToString
             }
             
         }
@@ -208,7 +207,7 @@ function Get-IPNetID {
     [Net.IPAddress]$IP = $IP
     [Net.IPAddress]$netID = $IP.Address -band $SubnetMask.address
     Write-Host -Foreground "green" "The network ID for $IP is" $netID
-    $netID.gettype()
+    #$netID.gettype()
 
 }   
 
@@ -221,7 +220,7 @@ function Get-IPNetID {
 #>
 
 #* My function calculates the number of valid hosts for a given subnet
-function Valid-Hosts {
+function Get-ValidHosts {
     Param(
         [Parameter(Mandatory=$true)]
         [string] $cidrMask
@@ -233,4 +232,4 @@ function Valid-Hosts {
     Write-Host -ForegroundColor "green" "The number of valid hosts on the /$cidrMask subnet is: " $numValidHosts
 }
 
-#Valid-Hosts 19
+#Get-ValidHosts 19
