1ebaf7eef866fc8881a99ee4759eb49d24eb07dc
Dalton Cole <dacole@navexglobal.com>
2018-10-11 13:19:17 -0600

    Created new script file for deleting old screenshots

---
+++ ptcs_scripts/delete_old_scaps.ps1
@@ -0,0 +1,6 @@
+<#
+    This script will delete old screenshots from the GreenShot folder. Given the number of screenshots I take on a daily basis for clients, I need a way to minimize the amount of junk stored on my system.
+
+    GOALS
+    
+#>
