9ea064be97e3b9440cffe9dfa5909958efe4e577
Dalton Cole <dacole@navexglobal.com>
2019-02-25 12:00:30 -0700

    EDA script in progress

--- ptcs_scripts/bootleg_EDA_module.ps1
+++ ptcs_scripts/bootleg_EDA_module.ps1
@@ -1,1 +1,14 @@
-# test
+#===================================================
+# Program Name : Bootleg EDA Module
+# Author: <admin777>
+# Description: Script will take a list of document URLs from an Excel spreadsheet, insert the string required to initiate a download of the source file, then sign in to the system 
+# and export documents.
+#===================================================
+
+function Create-URLs {
+
+    $source_File = Read-Host -Prompt 'Please provide a full file path with the document URLs you want to download: '
+
+    
+    
+}
